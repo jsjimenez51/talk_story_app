6341c4a22f23d2093f6a424dd04087351a628ff7
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-06-25 09:12:54 -0700

    UPDATE: created chatroom class

---
+++ public/src/scripts/chat.js
@@ -0,0 +1,32 @@
+// adding new chat docs
+// setting up a real-time listener to get new chats
+// updating the username
+// updating the room
+
+class Chatroom {
+  constructor(room, username) {
+    this.room = room;
+    this.username = username;
+    this.chats = db.collection("chats"); //ref to chat collection in firebase
+  }
+  async addChat(message) {
+    // format a chat object
+    const now = new Date();
+    const chat = {
+      message,
+      username: this.username,
+      room: this.room,
+      created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+    // save the chat document
+    const response = await this.chats.add(chat);
+    return response;
+  }
+}
+
+const chatroom = new Chatroom("gaming", "mikaela");
+
+chatroom
+  .addChat("testing this")
+  .then(() => console.log("chat added"))
+  .catch(err => console.log(err));
