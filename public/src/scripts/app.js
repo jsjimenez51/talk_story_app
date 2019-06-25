547ab03ebc833c4a67e244cec9b1c0b2f3f9b801
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-06-25 11:13:44 -0700

    UPDATE: added updating name feature w/localStore

--- public/src/scripts/app.js
+++ public/src/scripts/app.js
@@ -1,6 +1,8 @@
 // DOM queries
 const chatList = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
+const newNameForm = document.querySelector(".new-name");
+const updateMsg = document.querySelector(".update-mssg");
 
 // add a new chat
 newChatForm.addEventListener("submit", e => {
@@ -12,9 +14,25 @@ newChatForm.addEventListener("submit", e => {
     .catch(err => console.log(err));
 });
 
+// update username
+newNameForm.addEventListener("submit", e => {
+  e.preventDefault();
+  // update name via Chatroom class
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  // reset form
+  newNameForm.reset();
+  // show then hide the update message
+  updateMsg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => (updateMsg.innerText = ""), 2000);
+});
+
+// check local storage for existing username
+const username = localStorage.username ? localStorage.username : "Anonymous";
+
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom("general", "mikaela");
+const chatroom = new Chatroom("general", username);
 
 // get chats and render
 chatroom.getChats(data => chatUI.render(data));
--- public/src/scripts/chat.js
+++ public/src/scripts/chat.js
@@ -38,6 +38,7 @@ class Chatroom {
   }
   updateName(username) {
     this.username = username;
+    localStorage.setItem("username", username);
   }
   updateRoom(room) {
     this.room = room;
