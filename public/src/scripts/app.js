2ce0f75de70c2d178c760d863e74dda21b3961ed
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-06-25 10:36:24 -0700

    UPDATE: added new chats feature

--- public/src/scripts/app.js
+++ public/src/scripts/app.js
@@ -1,11 +1,20 @@
 // DOM queries
 const chatList = document.querySelector(".chat-list");
+const newChatForm = document.querySelector(".new-chat");
+
+// add a new chat
+newChatForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom
+    .addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err));
+});
 
 // class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom("general", "mikaela");
 
 // get chats and render
-chatroom.getChats(data => {
-  chatUI.render(data);
-});
+chatroom.getChats(data => chatUI.render(data));
