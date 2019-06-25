4a97f8ae9cc2a93bc873a42d069f9dbda04fe6f8
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-06-25 09:25:15 -0700

    UPDATE: added realtime listener functionality

--- public/src/scripts/chat.js
+++ public/src/scripts/chat.js
@@ -22,11 +22,20 @@ class Chatroom {
     const response = await this.chats.add(chat);
     return response;
   }
+  getChats(callback) {
+    this.chats.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if (change.type === "added") {
+          // update the ui
+          callback(change.doc.data());
+        }
+      });
+    });
+  }
 }
 
 const chatroom = new Chatroom("gaming", "mikaela");
 
-chatroom
-  .addChat("testing this")
-  .then(() => console.log("chat added"))
-  .catch(err => console.log(err));
+chatroom.getChats(data => {
+  console.log(data);
+});
