53542dea6f96a237e01bc2ad4c588a659d91c983
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-06-25 09:53:51 -0700

    UPDATE: added in change room/username features

--- public/src/scripts/chat.js
+++ public/src/scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
     this.room = room;
     this.username = username;
     this.chats = db.collection("chats"); //ref to chat collection in firebase
+    this.unsub;
   }
   async addChat(message) {
     // format a chat object
@@ -23,7 +24,7 @@ class Chatroom {
     return response;
   }
   getChats(callback) {
-    this.chats
+    this.unsub = this.chats
       .where("room", "==", this.room)
       .orderBy("created_at")
       .onSnapshot(snapshot => {
@@ -35,6 +36,16 @@ class Chatroom {
         });
       });
   }
+  updateName(username) {
+    this.username = username;
+  }
+  updateRoom(room) {
+    this.room = room;
+    console.log("room updated");
+    if (this.unsub) {
+      this.unsub();
+    }
+  }
 }
 
 const chatroom = new Chatroom("general", "mikaela");
@@ -42,3 +53,12 @@ const chatroom = new Chatroom("general", "mikaela");
 chatroom.getChats(data => {
   console.log(data);
 });
+
+setTimeout(() => {
+  chatroom.updateRoom("gaming");
+  chatroom.updateName("rene");
+  chatroom.getChats(data => {
+    console.log(data);
+  });
+  chatroom.addChat("timeout chatter");
+}, 3000);
